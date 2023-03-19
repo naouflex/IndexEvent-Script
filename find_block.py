dec8ffe746a8a485ff5c9f1d40f153008c88b193
naouflex <naoufel.eth@gmail.com>
2023-03-19 10:24:01 +0100

    Add contract and start_block parameters

--- find_block.py
+++ find_block.py
@@ -5,27 +5,35 @@ from multiprocessing import Process, Manager
 import math
 import time
 import os, sys
+import argparse
 from dotenv import load_dotenv
 load_dotenv()
 
+parser = argparse.ArgumentParser(description="Find the block where the contract was deployed")
+parser.add_argument("contract_address", help="Contract address")
+parser.add_argument("-s","--start_block", type=int, default=0, help="Block number to start scanning from (default: 0)")
+args = parser.parse_args()
+
 try:
+    start_block = args.start_block
+    print("Start block: ", start_block)
+    contract_address = Web3.toChecksumAddress(args.contract_address)
     # Initialize Web3 provider
     provider = Web3(Web3.HTTPProvider(os.getenv('RPC_URL')))
-
-    # Set contract address
-    contract_address = Web3.toChecksumAddress("0x41d5d79431a913c4ae7d69a668ecdfe5ff9dfb68")
 
     # Set maximum block number to search (currently set to the latest block)
     max_block_number = provider.eth.blockNumber
 
     # Set number of threads or processes to use
-    num_threads_or_processes = 10
+    num_threads_or_processes = 1
 
     # Set up progress bar with a miniters value of 10
     pbar = tqdm(total=max_block_number, miniters=10)
 
     # Create a lock for progress bar updates
     pbar_lock = Lock()
+        # Create a lock for result_list updates
+    result_list_lock = Lock()
 
     # Define function to search for contract in a range of blocks
     def search_for_contract(start_block, end_block, result_list):
@@ -36,6 +44,8 @@ try:
             for block_number in range(batch_start, batch_end + 1):
                 # Get block
                 block = provider.eth.getBlock(block_number)
+                
+                print(block_number)
                 # Loop through transactions in block
                 for tx_hash in block['transactions']:
                     # Get transaction receipt
@@ -43,7 +53,8 @@ try:
                     # Check if contract was created in this transaction
                     if tx_receipt and tx_receipt['contractAddress'] and provider.eth.getCode(tx_receipt['contractAddress']) == provider.eth.getCode(contract_address):
                         # Contract was created in this transaction, so add block number to result list and exit
-                        result_list.append(block_number)
+                        with result_list_lock:
+                            result_list.append(block_number)
                         return
             # Increment the progress bar after each batch of blocks is searched
             with pbar_lock:
@@ -51,10 +62,10 @@ try:
 
     # Divide the blocks into ranges for each thread or process
     ranges = []
-    block_range_size = math.ceil(max_block_number / num_threads_or_processes)
+    block_range_size = math.ceil((max_block_number - start_block + 1) / num_threads_or_processes)
 
     for i in range(num_threads_or_processes):
-        start_block = i * block_range_size
+        start_block = start_block + i * block_range_size
         end_block = min(start_block + block_range_size - 1, max_block_number)
         ranges.append((start_block, end_block))
 
@@ -62,7 +73,7 @@ try:
     if num_threads_or_processes == 1:
         # Use a single thread
         result_list = []
-        search_for_contract(0, max_block_number, result_list)
+        search_for_contract(start_block, max_block_number, result_list)
     else:
         # Use multiple threads or processes
         manager = Manager()
